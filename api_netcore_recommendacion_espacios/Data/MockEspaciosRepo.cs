c04c77beb54e284b63167987e1c2966d190a7798
jpablo9988 <65927907+jpablo9988@users.noreply.github.com>
2022-05-26 15:23:12 -0500

    toString and added new attribute

--- api_netcore_recommendacion_espacios/Data/MockEspaciosRepo.cs
+++ api_netcore_recommendacion_espacios/Data/MockEspaciosRepo.cs
@@ -13,36 +13,81 @@ namespace api_netcore_recommendacion_espacios.Data
 
         public Espacios_Test GetEspaciosById(int id)
         {
-            for ( int i = 0; i < espacios.Count; i++ )
+            for (int i = 0; i < espacios.Count; i++)
             {
                 if (espacios[i].Id == id)
                 {
                     return espacios[i];
                 }
             }
-            return null;    
+            return null;
         }
 
-        public void CreateData ()
+        public void CreateData()
         {
             espacios = new List<Espacios_Test>
             {
-                new Espacios_Test { Id = 0000, 
+                new Espacios_Test { Id = 0000,
                     Name = "Eduardo Martinez 505",
                     Desc = "Edificio Universidad Prueba",
                     X = 56.435,
-                    Y = 23.646},
+                    Y = 23.646,
+                    IdEstudiante = 0002028150},
                 new Espacios_Test { Id = 0001,
                     Name = "Matematicas 202",
                     Desc = "Edificio Universidad Prueba",
                     X = 53.128,
-                    Y = 21.989},
+                    Y = 21.989,
+                    IdEstudiante = 0002034150},
                 new Espacios_Test { Id = 0002,
                     Name = "Física 2102",
                     Desc = "Edificio Universidad Prueba",
                     X = 50.001,
-                    Y = 28.109}
+                    Y = 28.109,
+                    IdEstudiante = 00020128150}
             };
+        }
+        public string ToJsonString(int id = -1)
+        {
+            string jsonString = "\"Estudiantes\":";
+            if (id == -1)
+            {
+                jsonString += "[";
+                for (int i = 0; i < espacios.Count; i++)
+                {
+                    if (i > 0) jsonString += ",";
+                    jsonString += "{";
+                    jsonString += "\"Id\":" + this.espacios[i].Id.ToString() + ",";
+                    jsonString += "\"Name\":\"" + this.espacios[i].Name + "\",";
+                    jsonString += "\"Desc\":\"" + this.espacios[i].Desc + "\",";
+                    jsonString += "\"X\":" + this.espacios[i].X.ToString() + ",";
+                    jsonString += "\"Y\":" + this.espacios[i].Y.ToString() + ",";
+                    jsonString += "\"IdEstudiante\":" + this.espacios[i].IdEstudiante.ToString() + ",";
+                    jsonString += "}";
+                }
+                jsonString += "}]";
+                return jsonString;
+            }
+            else
+            {
+                for (int i = 0; i < espacios.Count; i++)
+                {
+                    if (this.espacios[i].Id == id)
+                    {
+                        jsonString += "{";
+                        jsonString += "\"Id\":" + this.espacios[i].Id.ToString() + ",";
+                        jsonString += "\"Name\":\"" + this.espacios[i].Name + "\",";
+                        jsonString += "\"Desc\":\"" + this.espacios[i].Desc + "\",";
+                        jsonString += "\"X\":" + this.espacios[i].X.ToString() + ",";
+                        jsonString += "\"Y\":" + this.espacios[i].Y.ToString() + ",";
+                        jsonString += "\"IdEstudiante\":" + this.espacios[i].IdEstudiante.ToString() + ",";
+                        jsonString += "}";
+                    }
+                    return jsonString;
+                }
+
+            }
+            return null;
         }
     }
 }
